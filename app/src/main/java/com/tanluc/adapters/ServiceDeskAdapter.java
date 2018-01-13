f035df1a30471f9a897a16e9f0ed7cf66ac49f17
luclegen <huynhtanluc.auto@gmail.com>
2018-01-13 15:36:35 +0700

    Create new three fields: context: screen use Layout with Activity type, resource: Layout for each row with int type, objects: Source data with int type List<ServiceDesk> in the ServiceDeskAdapter class

--- app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
+++ app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
@@ -1,7 +1,19 @@
 package com.tanluc.adapters;
 
+import android.app.Activity;
+
+import com.tanluc.models.ServiceDesk;
+
+import java.util.List;
+
 /**
  * Created by TANLUC on 13/01/2018.
  */
 public class ServiceDeskAdapter {
+
+    Activity context;               //Screen use Layout
+    int resource;                   //Layout for each row
+    List<ServiceDesk> objects;      //Source data
+
+
 }
