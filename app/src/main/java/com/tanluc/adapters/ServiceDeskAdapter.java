c63289839955887f16e0ce2fc56c244019b1da28
luclegen <huynhtanluc.auto@gmail.com>
2018-01-13 15:40:56 +0700

    Remove a non-argument constructor and all getters and setters in ServiceDeskAdapter class

--- app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
+++ app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
@@ -15,36 +15,10 @@ public class ServiceDeskAdapter {
     int resource;                   //Layout for each row
     List<ServiceDesk> objects;      //Source data
 
-    public ServiceDeskAdapter() {
-    }
-
     public ServiceDeskAdapter(Activity context, int resource, List<ServiceDesk> objects) {
         this.context = context;
         this.resource = resource;
         this.objects = objects;
     }
 
-    public Activity getContext() {
-        return context;
-    }
-
-    public void setContext(Activity context) {
-        this.context = context;
-    }
-
-    public int getResource() {
-        return resource;
-    }
-
-    public void setResource(int resource) {
-        this.resource = resource;
-    }
-
-    public List<ServiceDesk> getObjects() {
-        return objects;
-    }
-
-    public void setObjects(List<ServiceDesk> objects) {
-        this.objects = objects;
-    }
 }
