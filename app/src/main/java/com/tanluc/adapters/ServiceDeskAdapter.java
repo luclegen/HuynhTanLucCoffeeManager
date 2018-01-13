6c99f6514bb4b28f9a5e31a05d221f164e683e48
luclegen <huynhtanluc.auto@gmail.com>
2018-01-13 15:51:49 +0700

    Update ServiceDeskAdapter class inhert ArrayAdapter<ServiceDesk> class and full constructor of it.

--- app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
+++ app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
@@ -1,6 +1,9 @@
 package com.tanluc.adapters;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.tanluc.models.ServiceDesk;
 
@@ -9,16 +12,16 @@ import java.util.List;
 /**
  * Created by TANLUC on 13/01/2018.
  */
-public class ServiceDeskAdapter {
+public class ServiceDeskAdapter extends ArrayAdapter<ServiceDesk> {
 
     Activity context;               //Screen use Layout
     int resource;                   //Layout for each row
     List<ServiceDesk> objects;      //Source data
 
     public ServiceDeskAdapter(Activity context, int resource, List<ServiceDesk> objects) {
+        super(context, resource, objects);
         this.context = context;
         this.resource = resource;
         this.objects = objects;
     }
-
 }
