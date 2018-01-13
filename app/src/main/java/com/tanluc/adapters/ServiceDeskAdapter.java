766461dfb030c2bd9e40e4c6a2a601c5a79703b7
luclegen <huynhtanluc.auto@gmail.com>
2018-01-13 16:45:05 +0700

    Create a new inflater object with LayoutInflater type - a class to build become Java code.

--- app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
+++ app/src/main/java/com/tanluc/adapters/ServiceDeskAdapter.java
@@ -1,6 +1,7 @@
 package com.tanluc.adapters;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +28,7 @@ public class ServiceDeskAdapter extends ArrayAdapter<ServiceDesk> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = this.context.getLayoutInflater(); //LayoutInflater: a class to build become Java code
         return super.getView(position, convertView, parent);
     }
 }
