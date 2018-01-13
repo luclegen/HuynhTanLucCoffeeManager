39a2d474fd25b157b2cc1bb64c6bd668adac51b8
luclegen <huynhtanluc.auto@gmail.com>
2018-01-13 17:35:58 +0700

    Declare a txtDeskNumber instance with EditText type, RadioGroup with id: "radGrSelect" and three instance: radDeskEmpty, radDeskAvailable, radDeskCleaning, radDeskBooked with RadioButton type.

--- app/src/main/java/com/tanluc/huynhtanluccoffeemanager/MainActivity.java
+++ app/src/main/java/com/tanluc/huynhtanluccoffeemanager/MainActivity.java
@@ -2,8 +2,15 @@ package com.tanluc.huynhtanluccoffeemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtDeskNumber;
+    RadioGroup radGrSelect;
+    RadioButton radDeskEmpty, radDeskAvailable, radDeskCleaning,radDeskBooked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
