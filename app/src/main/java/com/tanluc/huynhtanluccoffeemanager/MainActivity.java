7ccee8d7945fe24752f34ccaad3c62bf7bb74aec
luclegen <huynhtanluc.auto@gmail.com>
2018-01-14 22:52:19 +0700

    In the MainActivity class, modify code so that initalize txtStatus instance with value is "No index".

--- app/src/main/java/com/tanluc/huynhtanluccoffeemanager/MainActivity.java
+++ app/src/main/java/com/tanluc/huynhtanluccoffeemanager/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radGrSelect;
     RadioButton radDeskEmpty, radDeskAvailable, radDeskCleaning,radDeskBooked;
     Button btnSaveData;
-    String txtStatus;
+    String txtStatus = "No index";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
